5b7e8a6b2f85e074e13575b11b781581b7fcf639
EmmaLatham98 <emma@lathamit.co.uk>
2019-04-01 18:13:48 +0100

    View Events Has Navigation ~ Updated View Event for with navigation

--- app/src/main/java/com/scrum/plan/planbee/ViewEventActivity.java
+++ app/src/main/java/com/scrum/plan/planbee/ViewEventActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -58,6 +61,48 @@ public class ViewEventActivity extends AppCompatActivity {
         txtGroupName.setText(event.getGroupName());
         txtDescription.setText(event.getDescription());
         imgEvent.setImageResource(eventIcons[eventIndex]);
+
+        navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem navItem) {
+                int itemId = navItem.getItemId();
+                switch (itemId) {
+                    case R.id.nav_login:
+                        startActivity(new Intent(ViewEventActivity.this, LoginActivity.class));
+                        break;
+                    case R.id.nav_create_account:
+                        startActivity(new Intent(ViewEventActivity.this, CreateAccountActivity.class));
+                        break;
+                    case R.id.nav_group_chats:
+                        startActivity(new Intent(ViewEventActivity.this, GroupChats.class));
+                        break;
+                    case R.id.nav_map:
+                        startActivity(new Intent(ViewEventActivity.this, MapActivity.class));
+                        break;
+                    case R.id.nav_browse_events:
+                        startActivity(new Intent(ViewEventActivity.this, MainActivity.class));
+                        break;
+                    case R.id.nav_calendar:
+                        startActivity(new Intent(ViewEventActivity.this, CalendarActivity.class));
+                        break;
+                    case R.id.nav_my_events:
+                        startActivity(new Intent(ViewEventActivity.this, MyEventsActivity.class));
+                        break;
+                }
+                return false;
+            }
+        });
+
+        drawLayout = findViewById(R.id.drawer_layout);
+        btnOpenNav = findViewById(R.id.btn_open_nav);
+        btnOpenNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
 
     }
 }
